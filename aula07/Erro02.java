3085df45234612ab5df8ccaa7e29f3537cd88359
Rogério Souza <rogerio_dsouza@hotmail.com>
2017-09-21 17:20:14 -0300

    Java Errors

---
+++ aula07/Erro02.java
@@ -0,0 +1,12 @@
+class Erro02 {
+    
+        //erro de semantica
+       // public static void Main(String[] args) {
+            public static void main(String[] args) {
+        
+                int cont = 5;
+                        
+                System.out.println("Valor: " + cont);
+            
+        }
+    }
---
+++ aula07/Erro03.java
@@ -0,0 +1,11 @@
+class Erro03 {
+    
+    public static void main(String[] args) {
+                                       
+            //erro em tempo de execução
+
+            System.out.println("Resultado: " + 1/0);
+            
+        }
+   
+}
