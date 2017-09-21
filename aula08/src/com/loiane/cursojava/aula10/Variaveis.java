1505d1ef9e420359b4213cba15cef5f7278aea68
Rogério Souza <rogerio_dsouza@hotmail.com>
2017-09-21 19:26:16 -0300

    Variables: Introduction

---
+++ aula08/src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String nome = "Rogerio";
+		String nomeDoMeuCachorro = "totó";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do meu cachorro = " + nomeDoMeuCachorro);
+	}
+
+}
