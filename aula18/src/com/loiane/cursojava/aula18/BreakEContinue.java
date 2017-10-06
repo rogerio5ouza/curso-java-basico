7ee14bfc8940d326cd969288eee7235ab0016a5d
Rogério Souza <rogerio_dsouza@hotmail.com>
2017-10-05 21:27:34 -0300

    Break and Continue

---
+++ aula18/src/com/loiane/cursojava/aula18/BreakEContinue.java
@@ -0,0 +1,46 @@
+package com.loiane.cursojava.aula18;
+
+import java.util.Scanner;
+
+public class BreakEContinue {
+
+	public static void main(String[] args) {
+
+		// Comando Break
+
+		/*Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com um número:");
+		int num = scan.nextInt();
+
+		System.out.println("Entre com um limite:");
+		int max = scan.nextInt();
+
+		for (int i = num; i <= max; i++) {
+			System.out.println(i);
+			if (i % 7 == 0) {
+				System.out.println("O valor de i é: " + i);
+				break;
+			}
+		}*/
+
+		// Comando Continue
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com um número:");
+		int num = scan.nextInt();
+
+		System.out.println("Entre com um limite:");
+		int max = scan.nextInt();
+
+		for (int i = num; i <= max; i++) {
+			if (i % 7 == 0) {
+				continue;
+			}
+			System.out.println("O valor de i é: " + i);
+
+		}
+	}
+
+}
